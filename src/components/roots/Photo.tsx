7f5c89f508aad1408e48c04faed15feac75f5e97
“asifulhaque087” <asifulhaque087@gmail.com>
2023-11-03 04:43:31 +0600

    Update hover styles on select

--- src/components/roots/Photo.tsx
+++ src/components/roots/Photo.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import React, { forwardRef, CSSProperties, RefObject } from 'react';
 
 export const Photo = forwardRef<HTMLDivElement, IPhotoProps>(
-  ({ url, index, faded, style, removePhoto, ...props }, ref) => {
+  ({ url, index, faded, style, isSelected, selectPhotos, ...props }, ref) => {
     const inlineStyles: CSSProperties = {
       opacity: faded ? '0.2' : '1',
       transformOrigin: '0 0',
@@ -27,7 +27,7 @@ export const Photo = forwardRef<HTMLDivElement, IPhotoProps>(
         {...props}
         onClick={() => {
           console.log('hello world ');
-          if (removePhoto) removePhoto(index);
+          if (selectPhotos) selectPhotos(index, isSelected ? true : false);
         }}
       >
         <Image
@@ -37,8 +37,20 @@ export const Photo = forwardRef<HTMLDivElement, IPhotoProps>(
           layout="responsive"
           alt="..."
         />
-        <div className="absolute top-0 bottom-0 left-0 right-0  opacity-0 hover:opacity-100 hover:bg-black/60 transition-all p-[8px]">
-          <span className="block h-[15px] w-[15px] rounded bg-white" />
+        <div
+          className={`absolute top-0 bottom-0 left-0 right-0  ${
+            isSelected
+              ? 'backdrop-opacity-80 bg-white/30'
+              : 'opacity-0 hover:opacity-100 hover:bg-black/60'
+          }  transition-all p-[20px]`}
+        >
+          <div
+            className={`grid place-items-center text-white text-[10px] h-[20px] w-[15px] rounded ${
+              isSelected ? 'bg-[blue]' : 'bg-white'
+            }`}
+          >
+            ✓
+          </div>
         </div>
       </div>
     );
