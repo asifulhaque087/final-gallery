72451ef44079fed335450a1d0c50a4934617f59f
“asifulhaque087” <asifulhaque087@gmail.com>
2023-11-05 13:50:54 +0600

    Add type for header component

--- src/types/compounds/index.ts
+++ src/types/compounds/index.ts
@@ -1,4 +1,5 @@
 import { ISortablePhotoProps } from './SortablePhoto';
 import { IGrid } from './Grid';
+import { IHeader } from './Header';
 
-export type { ISortablePhotoProps, IGrid };
+export type { ISortablePhotoProps, IGrid, IHeader };
