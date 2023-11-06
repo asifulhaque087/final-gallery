addbd6c161c7c63478b4056d1f3f5851efedc630
“asifulhaque087” <asifulhaque087@gmail.com>
2023-11-06 11:57:00 +0600

    Remove unwanted component

--- src/components/roots/Button.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export const Button = () => {
-  return (
-    <button className="px-[15px] py-[7px] bg-indigo-700 rounded text-white">
-      Button
-    </button>
-  );
-};
--- src/components/roots/index.ts
+++ src/components/roots/index.ts
@@ -1,4 +1,3 @@
-import { Button } from './Button';
 import { Photo } from './Photo';
 
-export { Button, Photo };
+export { Photo };
